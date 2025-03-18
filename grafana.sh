ed97d25992a39e9e7b6c3062da3fe400e0bcb83e
Uffoh Chukwuka <forrestuffoh@gmail.com>
2025-03-18 11:00:50 +0100

    Grafana File

---
+++ grafana.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Update the system
+echo "Updating system packages..."
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+# Install Grafana
+sudo apt-get install -y software-properties-common
+echo "Installing Grafana..."
+curl https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sudo apt-get update -y && sudo apt-get install -y grafana
+
+#Starting the Grafana Service
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
+
+# sudo vim grafana.sh && sudo chmod +x grafana.sh
+
+sudo systemctl status grafana-server --no-pager
+
+# setup data source with git
+
+grafana-cli admin reset-admin-password 
+
+sudo systemctl restart grafana-server
+
+
+Create a data source in grafana with the endpoint of the network load balancer 
+and the port exposed on it (querier port: 10904)
+
+LB IP http://10.150.65.21:10904/
